e7a11410e4886a2dd9006fe69439df1f2dcb3895
Ellyn Butler <ellynrbutler@gmail.com>
2021-01-06 17:54:50 -0500

    now downloads

--- scripts/organize/downloadNewRaw.py
+++ scripts/organize/downloadNewRaw.py
@@ -12,16 +12,17 @@ import json
 import shutil
 import re
 import time
-import pandas as pd
-import pytz
+#import pandas as pd
+#import pytz
 import datetime
 import zipfile
-import numpy as np
+#import numpy as np
 
 
 
-#outdir = '/Users/butellyn/Documents/nscor/data/raw/'
-outdir = '/project/nscor/data/raw/' # Path on PMACS
+#outdir = '/Users/butellyn/Documents/nscor/data/raw/' # path on local computer
+#outdir = '/home/butellyn/nscor/data/raw/' # tmp path on PMACS
+#outdir = '/project/bbl_projects/nscor/data/raw/' # Path on PMACS
 
 fw = flywheel.Client()
 proj = fw.lookup('bbl/NASA_821559')
@@ -37,19 +38,31 @@ for subj in subjects:
     for ses in subj.sessions():
         ses = ses.reload()
         seslabel = ses['label']
-        sesdir = outdir+'sub-'+sublabel+'/ses-'+seslabel
+        sesdir = outdir+'sub-'+sublabel+'/ses-'+seslabel+'/'
         if not os.path.exists(sesdir):
             os.mkdir(sesdir)
         # Check if output directory is empty
         # if so, download the session zip and unzip it
         if len(os.listdir(sesdir)) == 0:
             if len(ses.files) > 0:
-                for file in ses.files:
+                for file in ses.files: # January 6, 2020: Files do not include niftis...
                     filename = file['name']
                     ses.download_file(filename, sesdir+filename)
                     # Unzip files
                     with zipfile.ZipFile(sesdir+filename,"r") as zip_ref:
                         zip_ref.extractall(sesdir)
+                    # Move all of the files into the created session directory
+                    file_names = os.listdir(sesdir+sublabel) #line "22"
+                    for file_name in file_names:
+                        shutil.move(os.path.join(sesdir+sublabel, file_name), sesdir)
+                    os.rmdir(sesdir+sublabel)
+                for acq in ses.acquisitions(): # January 6, 2020: Files do not include niftis...
+                    for file in acq['files']:
+                        filename = file['name']
+                        acq.download_file(filename, sesdir+filename)
+                        # Unzip files
+                        #with zipfile.ZipFile(sesdir+filename,"r") as zip_ref:
+                        #    zip_ref.extractall(sesdir)
             else:
                 print('sub-'+sublabel+' ses-'+seslabel+' has zero files')
         else:
